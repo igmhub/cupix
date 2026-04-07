3ea1e07c05d2a72c2671d3e74cc790dfbce923a8
marlena6 <mel3ca@virginia.edu>
2026-04-07 04:04:35 -0700

    small changes to chi2 script

--- scripts/chi2_scan.sh
+++ scripts/chi2_scan.sh
@@ -2,7 +2,6 @@
 #SBATCH --qos=debug
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
 #SBATCH --constraint=cpu
 #SBATCH --account=desi
 #SBATCH --output=/pscratch/sd/m/mlokken/desi-lya/px/logs/%x-%j.out
@@ -10,8 +9,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mlokken@ifae.es
 
-export OMP_NUM_THREADS=1
-
 module load python
 conda activate cupix
-python /global/common/software/desi/users/mlokken/cupix/scripts/chi2_scan.py igm tight mF T0
+cd $PSCRATCH/desi-lya/px/
+srun -n 128 -c 1 /global/common/software/desi/users/mlokken/cupix/scripts/chi2_scan_mock.py arinyo tight bias beta
