c608d19a1d6a68acd9788b7c90b8c6ae71d78ab8
Andreu Font <font.andreu@gmail.com>
2026-04-14 22:24:19 -0700

    added first version of posterior, free param and minimizer

---
+++ cupix/likelihood/free_parameter.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+class FreeParameter(object):
+    """Basic class describing a free parameter"""
+
+    def __init__(self,
+        name,
+        min_value=None,
+        max_value=None,
+        ini_value=None,
+        true_value=None,
+        gauss_prior_mean=None,
+        gauss_prior_width=None,
+        latex_label=None
+    ):
+        self.name = name
+        self.min_value = min_value
+        self.max_value = max_value
+        self.ini_value = ini_value
+        self.true_value = true_value
+        self.gauss_prior_mean = gauss_prior_mean
+        self.gauss_prior_width = gauss_prior_width
+        if latex_label is not None:
+            self.latex_label = latex_label
+        else:
+            self.latex_label = self.name
+
+        return
+
+
+    def get_log_prior(self, value):
+        assert self.gauss_prior_mean is not None
+        assert self.gauss_prior_width is not None
+        return ( (value-self.gauss_prior_mean) / self.gauss_prior_width)**2
+
---
+++ notebooks/examples/test_posterior.py
@@ -0,0 +1,165 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: cupix
+#     language: python
+#     name: cupix
+# ---
+
+# %% [markdown]
+# # Test the new posterior class
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py as h5
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+from lace.cosmo import cosmology
+from cupix.px_data.data_DESI_DR2 import DESI_DR2
+from cupix.likelihood.theory import Theory
+from cupix.likelihood.likelihood import Likelihood
+from cupix.likelihood.posterior import Posterior
+from cupix.likelihood.free_parameter import FreeParameter
+import cupix
+cupixpath = cupix.__path__[0].rsplit('/', 1)[0]
+
+# %% [markdown]
+# ### Step 1: Import a noiseless forecast
+
+# %%
+forecast_file = f"{cupixpath}/data/px_measurements/forecast/forecast_ffcentral_real_binned_out_px-zbins_4-thetabins_9_w_res_noiseless_z0.hdf5"
+forecast = DESI_DR2(forecast_file, kM_max_cut_AA=1, km_max_cut_AA=1.2)
+iz = 0
+z = forecast.z[iz]
+
+# %%
+true_cosmo_params = {}
+with h5.File(forecast_file) as f:
+    for key in f['cosmo_params'].attrs.keys():
+        true_cosmo_params[key] = f['cosmo_params'].attrs[key]
+print(true_cosmo_params)
+
+# %%
+# translate these to our Lya params
+true_lya_params = {}
+with h5.File(forecast_file) as f:
+    true_lya_params['bias'], = -1.0 * f['arinyo_pars'].attrs['bias_0'],
+    true_lya_params['beta'], = f['arinyo_pars'].attrs['beta_0'],
+    true_lya_params['av'], = f['arinyo_pars'].attrs['av_0'],
+    true_lya_params['bv'], = f['arinyo_pars'].attrs['bv_0'],
+    true_lya_params['kp_Mpc'], = f['arinyo_pars'].attrs['kp_0'],
+    true_lya_params['q1'], = f['arinyo_pars'].attrs['q1_0'],
+    true_lya_params['q2'], = f['arinyo_pars'].attrs['q2_0'],
+    true_lya_params['kv_Mpc'] = np.exp( np.log(f['arinyo_pars'].attrs['kvav_0']) / f['arinyo_pars'].attrs['av_0'] )
+print(true_lya_params)
+
+# %%
+# use the true cosmology as fiducial
+cosmo = cosmology.Cosmology(cosmo_params_dict=true_cosmo_params)
+
+# %%
+# use the true Lya parameters (Arinyo / bias / beta)
+config = true_lya_params | {'verbose': True}
+# make your life a bit harder by changing a bit the value of beta
+wrong_beta = False
+if wrong_beta:
+    config['beta'] = 1.02*config['beta']
+print(config)
+theory = Theory(z=z, fid_cosmo=cosmo, config=config)
+
+# %%
+like = Likelihood(data=forecast, theory=theory, iz=iz, verbose=True)
+
+# %%
+like.get_chi2()
+
+# %%
+# set the likelihood parameters as the Arinyo params with some fiducial values
+bias = FreeParameter(
+    name='bias',
+    min_value=-0.5,
+    max_value=-0.01,
+    ini_value=-0.1,
+    true_value=true_lya_params['bias'],
+    gauss_prior_mean=true_lya_params['bias'],
+    gauss_prior_width=0.02,    
+)
+beta = FreeParameter(
+    name='beta',
+    min_value=0.1,
+    max_value=5.0,
+    ini_value=1.5,
+    true_value=true_lya_params['beta'],
+    gauss_prior_mean=true_lya_params['beta'],
+    gauss_prior_width=0.2,    
+)
+free_params = [bias, beta]
+for par in free_params:
+    print(par.name, par.true_value)
+
+
+# %%
+post = Posterior(like, free_params, config={'verbose': True})
+
+# %%
+params = {'bias': -0.1, 'beta': 1.5}
+
+# %%
+post.get_log_posterior(params=params)
+
+# %%
+post.get_log_posterior()
+
+# %%
+post.get_log_prior()
+
+# %%
+from cupix.likelihood.minimize_posterior import Minimizer
+
+# %%
+mini = Minimizer(post, config={'verbose':True})
+
+# %%
+true_params = {'bias': bias.true_value, 'beta': beta.true_value}
+true_chi2 = post.like.get_chi2(params=true_params)
+true_post = post.get_log_posterior(params=true_params)
+print(true_chi2, true_post, -0.5*true_chi2)
+
+# %%
+betas = np.linspace(beta.true_value-0.1, beta.true_value+0.1, 11)
+chi2 = [post.like.get_chi2(params={'beta': beta}) for beta in betas]
+plt.plot(betas, chi2)
+plt.axvline(x=beta.true_value, color='gray', ls=':')
+
+# %%
+betas = np.linspace(beta.true_value-0.1, beta.true_value+0.1, 11)
+log_post = [post.get_log_posterior(params={'bias': bias.true_value, 'beta': val}) for val in betas]
+plt.plot(betas, log_post)
+plt.axvline(x=beta.true_value, color='gray', ls=':')
+
+# %%
+mini.silence()
+mini.verbose=True
+mini.post.verbose=True
+mini.minimize()
+
+# %%
+best_params = mini.get_best_fit_params()
+print(best_params)
+best_chi2 = like.get_chi2(params=best_params)
+print(best_chi2)
+
+# %%
+mini.plot_ellipses('bias','beta')
+
+# %%
